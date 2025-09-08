9350b23309d8299d9fad1604b0c32dbccd3b3e5a
faizal <jai.crys@gmail.com>
2025-09-08 22:40:03 +0800

    Update Tailwind CSS version to 4.1.10 in configuration

--- config/config.exs
+++ config/config.exs
@@ -43,7 +43,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "4.1.7",
+  version: "4.1.10",
   preline_phoenix_test: [
     args: ~w(
       --input=assets/css/app.css
